a3e3c4bd726905a8f3c404aa291c8b35295c3505
Nicolas Persivale <67163082+NPersivale@users.noreply.github.com>
2022-02-21 22:40:03 -0300

    second challenge upload

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () =>{
+    return (
+        <footer>
+            Soy Footer!
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const  NavBar = () => {
+    return (
+    <header>
+        <h1>Justgaming.com</h1>
+        <nav>
+            <a href="#">Primer Link</a>
+            <a href="#">Segundo Link</a>
+            <a href="#">Tercer Link</a>
+        </nav>
+    </header>
+    )
+}
+
+export default NavBar;
