09b47fd00121bafa9adc9f487e92c0859dcc190b
Nicolas Persivale <67163082+NPersivale@users.noreply.github.com>
2022-02-23 10:43:57 -0300

    added bootstrap and SASS

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,44 @@
-const  NavBar = () => {
+import { Container, Navbar, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
+
+const NavBar = () => {
     return (
-    <header>
-        <h1>Justgaming.com</h1>
-        <nav>
-            <a href="#">Primer Link</a>
-            <a href="#">Segundo Link</a>
-            <a href="#">Tercer Link</a>
-        </nav>
-    </header>
-    )
-}
+        <Navbar className="navbar" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Nav.Link href="#action2">Link</Nav.Link>
+                        <NavDropdown title="Link" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action5">
+                                Something else here
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#" disabled>
+                            Link
+                        </Nav.Link>
+                    </Nav>
+                    <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
 
 export default NavBar;
