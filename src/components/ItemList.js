d5420a09ea8f87e5dbbf8ca87e2dcef7376e0f7f
Nicolas Persivale <67163082+NPersivale@users.noreply.github.com>
2022-03-03 19:53:43 -0300

    added CSS styling

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,13 +1,14 @@
 import Item from "./Item"
 import { useState, useEffect } from "react"
-import HourglassTopOutlinedIcon from '@mui/icons-material/HourglassTopOutlined';
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 let productsInitial = [
     { name: "iRacing Hoodie", img: "/assets/hoodie-iracing.jpg", price: 35, stock: 10 },
-    {name: "EFT T-Shirt", img: "/assets/tshirt-eft.jpg", price: 25, stock: 10},
-    {name: "Star Citizen Collectors Drink Set", img: "/assets/collectorsdrinkset-SC.png", price: 89, stock: 2},
-    {name: "EFT Killa Mug", img: "/assets/killamug-eft.png", price: 19, stock: 15},
-    {name: "Star Citizen Backpack", img: "/assets/SC-backpack.jpg", price: 30, stock: 10}
+    { name: "EFT T-Shirt", img: "/assets/tshirt-eft.jpg", price: 25, stock: 10 },
+    { name: "Star Citizen Collectors Drink Set", img: "/assets/collectorsdrinkset-SC.png", price: 89, stock: 2 },
+    { name: "EFT Killa Mug", img: "/assets/killamug-eft.png", price: 19, stock: 15 },
+    { name: "Star Citizen Backpack", img: "/assets/SC-backpack.jpg", price: 30, stock: 10 }
 ]
 
 const ItemList = () => {
@@ -20,12 +21,11 @@ const ItemList = () => {
             setProducts(productsInitial);
             setLoading(false);
         }, 2000)
-    }, []
-    );
+    }, []);
 
     return (
         <>
-            {loading ? <h2 id="loading"><HourglassTopOutlinedIcon fontSize="large"/>Loading, please wait...</h2> : null}
+            {loading ? <div id="loading"><CircularProgress color="inherit" /><h2>Loading, please wait...</h2></div> : null}
             {products.map((product) => {
                 return <Item name={product.name} price={product.price} stock={product.stock} img={product.img} />
             })}
