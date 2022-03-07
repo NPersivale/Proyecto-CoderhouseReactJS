f3ee99482764da068dde086c6a5c024c6108d7fe
Nicolas Persivale <67163082+NPersivale@users.noreply.github.com>
2022-03-07 19:43:51 -0300

    swapped navbar for MUI

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import { React , useState } from "react"
 import ItemListContainer from "./ItemListContainer";
+import BackToTop from "./BackToTop";
 
 const Main = () => {
 
     return (
         <main>
+            <BackToTop/>
             <ItemListContainer/>
         </main>
     )
