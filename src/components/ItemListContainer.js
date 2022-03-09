663c40ea7546460466662db289d4420f1a7e8b18
Nicolas Persivale <67163082+NPersivale@users.noreply.github.com>
2022-03-09 20:34:54 -0300

    added products

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -8,7 +8,12 @@ let productsInitial = [
     { id: 2, name: "EFT T-Shirt", img: "/assets/tshirt-eft.jpg", price: 25, stock: 10 },
     { id: 3, name: "Star Citizen Collectors Drink Set", img: "/assets/collectorsdrinkset-SC.png", price: 89, stock: 2 },
     { id: 4, name: "EFT Killa Mug", img: "/assets/killamug-eft.png", price: 19, stock: 15 },
-    { id: 5, name: "Star Citizen Backpack", img: "/assets/SC-backpack.jpg", price: 30, stock: 10 }
+    { id: 5, name: "Star Citizen Backpack", img: "/assets/SC-backpack.jpg", price: 30, stock: 10 },
+    { id: 6, name: "iRacing 30Oz Tumbler", img: "/assets/30oztumbler-iracing.png", price: 25, stock: 15 },
+    { id: 7, name: "Star Citizen Stainless Steel Mug", img: "/assets/stainlessmug-SC.png", price: 29, stock: 10 },
+    { id: 8, name: "iRacing T-Shirt", img: "/assets/tshirt-iracing.jpg", price: 25, stock: 10 },
+    { id: 9, name: "EFT Labs Mug", img: "/assets/labsmug-eft.png", price: 19, stock: 10 },
+    { id: 10, name: "Star Citizen RSI Mug", img: "/assets/rsimug-SC.png", price: 19, stock: 10 }
 ]
 
 const ItemListContainer = () => {
