123e9c827a6778d1c9609af6705efc1455a15224
Nicolas Persivale <67163082+NPersivale@users.noreply.github.com>
2022-03-30 19:50:11 -0300

    added localstorage persistency

--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -1,14 +1,32 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 import { toast } from "react-toastify";
 
 export const cartContext = createContext([]);
 const { Provider } = cartContext;
 
 const CartProvider = ({ children }) => {
-
     const [cart, setCart] = useState([]);
     const [totalPrice, setTotalPrice] = useState(0);
     const [totalProds, setTotalProds] = useState(0);
+
+    useEffect(() => {
+        const cart = JSON.parse(localStorage.getItem("cart"));
+        if (cart) {
+            setCart(cart);
+            let totalPrice = 0;
+            let totalProds = 0;
+            cart.forEach(item => {
+                totalPrice += item.product.price * item.count;
+                totalProds += item.count;
+            });
+            setTotalPrice(totalPrice);
+            setTotalProds(totalProds);
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }, [cart]);
 
     const cartCheckout = (orderId) => {
         toast.success("Congratulations! Your purchase has been completed! The order id is: " + orderId, { autoClose: false, });
@@ -53,6 +71,7 @@ const CartProvider = ({ children }) => {
         setCart([]);
         setTotalPrice(0);
         setTotalProds(0);
+        localStorage.removeItem("cart");
         toast.info("Cart deleted.");
     }
 
