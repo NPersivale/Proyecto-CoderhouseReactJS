e6d83c15a1f5b7a4ee36c331a7103f78a3c494cd
Nicolas Persivale <67163082+NPersivale@users.noreply.github.com>
2022-03-30 20:24:47 -0300

    refactored code and added optimizations on cartContext

--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -13,26 +13,35 @@ const CartProvider = ({ children }) => {
         const cart = JSON.parse(localStorage.getItem("cart"));
         if (cart) {
             setCart(cart);
-            let totalPrice = 0;
-            let totalProds = 0;
-            cart.forEach(item => {
-                totalPrice += item.product.price * item.count;
-                totalProds += item.count;
-            });
-            setTotalPrice(totalPrice);
-            setTotalProds(totalProds);
+            calculateTotals(cart);
         }
     }, []);
 
     useEffect(() => {
+        calculateTotals(cart);
         localStorage.setItem("cart", JSON.stringify(cart));
     }, [cart]);
 
-    const cartCheckout = (orderId) => {
-        toast.success("Congratulations! Your purchase has been completed! The order id is: " + orderId, { autoClose: false, });
+    const calculateTotals = (cart) => {
+        let totalPrice = 0;
+        let totalProds = 0;
+        cart.forEach(item => {
+            totalPrice += item.product.price * item.count;
+            totalProds += item.count;
+        });
+        setTotalPrice(totalPrice);
+        setTotalProds(totalProds);
+    }
+
+    const clearCartStates = () => {
         setCart([]);
         setTotalPrice(0);
         setTotalProds(0);
+    }
+
+    const cartCheckout = (orderId) => {
+        toast.success("Congratulations! Your purchase has been completed! The order id is: " + orderId, { autoClose: false, });
+        clearCartStates();
     }
 
 
@@ -54,44 +63,19 @@ const CartProvider = ({ children }) => {
             tempCart = [cartProduct, ...cart]
             setCart(tempCart)
         }
-
-        let tempTotalPrice = 0;
-        let tempTotalProds = 0;
-
-        tempTotalPrice = totalPrice;
-        tempTotalPrice += (product.price * count);
-        setTotalPrice(tempTotalPrice);
-
-        tempTotalProds = totalProds;
-        tempTotalProds += count;
-        setTotalProds(tempTotalProds);
     }
 
     const clearCart = () => {
-        setCart([]);
-        setTotalPrice(0);
-        setTotalProds(0);
+        clearCartStates();
         localStorage.removeItem("cart");
         toast.info("Cart deleted.");
     }
 
     const deleteFromCart = (product) => {
         if (isInCart(product)) {
-            let tempTotalPrice = 0;
-            let tempTotalProds = 0;
             const tempCart = cart.filter(item => item.product !== product);
-
-            tempCart.forEach((item) => {
-                tempTotalPrice += (item.product.price * item.count);
-                setTotalPrice(tempTotalPrice);
-
-                tempTotalProds += item.count;
-                setTotalProds(tempTotalProds);
-            })
-
             setCart(tempCart);
             toast.info(product.name + " removed from your cart.");
-
             if (tempCart.length == 0) {
                 clearCart();
             }
